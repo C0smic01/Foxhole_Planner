34fb3fd47bbefec27720e45eca060166e44c0f38
C0smic <vovantung0212@gmail.com>
2025-02-09 21:48:59 +0700

    ---

---
+++ src/main/java/com/example/foxholeplanner/FoxholePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.foxholeplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxholePlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxholePlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foxholeplanner/FoxholePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foxholeplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxholePlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
